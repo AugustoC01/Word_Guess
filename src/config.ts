1c34908010c77717adfad95ee098d21732eb15d8
Augusto <augustocannataf@gmail.com>
2023-02-10 18:05:38 -0300

    Cors fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,19 @@
 const PORT = process.env.PORT || 8080;
 const NODE_ENV = process.env.NODE_ENV || "development";
+const FRONT_URL = process.env.FRONT_URL || "http://localhost:3000/";
 
-export { PORT, NODE_ENV };
+const corsOptions = {
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
+  ],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: FRONT_URL,
+  preflightContinue: false,
+};
+
+export { PORT, NODE_ENV, corsOptions };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from "cors";
+import { corsOptions, PORT } from "./config";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors(corsOptions));
 
 import Router from "./routes/routes";
 Router(app);
 
-import { PORT } from "./config";
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
