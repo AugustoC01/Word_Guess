19621ac212245df053df528df80617ae6ea56ff6
Augusto <augustocannataf@gmail.com>
2023-02-10 13:49:42 -0300

    Cors opened

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 import Router from "./routes/routes";
 Router(app);
 
 import { PORT } from "./config";
 app.listen(PORT, () => {
-  console.log(`Server on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
